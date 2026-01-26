26877653e1aee4740bf087f3a2c201e676330d55
kenj-matsumoto <kenj-matsumoto@cnt.biglobe.co.jp>
2026-01-26 16:29:43 +0900

    java25, springboot4

--- src/test/java/pj/sample/HelloApiTest.java
+++ src/test/java/pj/sample/HelloApiTest.java
@@ -1,21 +1,29 @@
 package pj.sample;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import pj.boot.BootApplication;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = BootApplication.class)
-@AutoConfigureMockMvc
 class HelloApiTest {
 
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
     @Test
     void hello_shouldReturnHelloMessage() throws Exception {
